526a199ce72f9ec1d7689afcc8f3d7c52dbddede
Brennen Gabriel <bgabr001@odu.edu>
2026-05-28 13:41:34 -0400

    update

--- FoundationsOfCodingFullStack/switchStatements/obj/Release/net7.0/switchStatements.AssemblyInfo.cs
+++ FoundationsOfCodingFullStack/switchStatements/obj/Release/net7.0/switchStatements.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("switchStatements")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c05761e0287512aac37fdff492c2117efc597f51")]
 [assembly: System.Reflection.AssemblyProductAttribute("switchStatements")]
